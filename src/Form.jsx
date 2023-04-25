3d018a7bd1349f03b336494296671c35f76564cf
ChelseyReann <chelseyreann@gmail.com>
2023-04-25 11:07:41 -0400

    includes state for form components

--- src/Form.jsx
+++ src/Form.jsx
@@ -2,13 +2,50 @@ import { useState } from 'react'
 
 function Form (){
     const [form, setForm] = useState('')
+    const [name, setName] = useState('')
+    const [phone, setPhone] = useState('')
+    const [url, setUrl] = useState('')
+    const [state, setState] = useState('')
+    const [country, setCountry] = useState('')
+
+    const handleName = (e) => {
+        setName(e.target.value)
+        console.log(e.target.value)
+    }
+
+    const handlePhone = (e) => {
+        setPhone(e.target.value)
+    }
+
+    const handleUrl = (e) => {
+        setUrl(e.target.value)
+    }
+
+    const handleState = (e) => {
+        setState(e.target.value)
+    }
+
+    const handleCountry = (e) => {
+        setCountry(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setForm()
+    } 
 
     return (
         <div>
-            <form>
-                <input type='text'></input>
-                <input type='submit'></input>
-            </form>
+            <h3>Know of a brewery that should be added to this list?</h3>
+            <h4>Fill out the form below to get it included!</h4>
+                <form>
+                    <input type='text' placeholder='Brewery Name' value={name} onChange={handleName}></input><br />
+                    <input type='text' placeholder='Brewery Phone Number' value={phone} onChange={handlePhone}></input><br />
+                    <input type='text' placeholder='Brewery Website' value={url} onChange={handleUrl}></input><br />
+                    <input type='text' placeholder='State' value={state} onChange={handleState}></input><br />
+                    <input type='text' placeholder='Country' value={country} onChange={handleCountry}></input><br />
+                    <input type='submit' onClick={handleSubmit}></input>
+                </form>
         </div>
     )
 }
